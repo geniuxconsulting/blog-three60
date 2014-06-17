b373b32a363a184b7e6fb3e4a625cfc3f35f95c2
Matyas Albert Nagy <matyas.albert@geniuxconsulting.de>
2014-06-17 16:16:02 +0200

    removed extra js files refactored whole thing for better readability & understandability

---
+++ public/js/main.js
@@ -0,0 +1,58 @@
+var PAN_VELOCITY = 0.005;
+
+
+// resets zoom
+var reset = function() {
+	three60.zoom(1);
+};
+
+// hammer and normal event listeners
+// hold previous touch  center. Important for calculating the delta movement values
+var prevCenter;
+var listeners = {
+	hammer: {
+		tap: reset,
+		dragstart: function(e) {
+			prevCenter = e.gesture.center;
+		},
+		drag: function(e) {
+			var dx = e.gesture.center.pageX - prevCenter.pageX;
+			var dy = e.gesture.center.pageY - prevCenter.pageY;
+			prevCenter = e.gesture.center;
+
+			three60.pan({
+				x: -dx * PAN_VELOCITY,
+				y: -dy * PAN_VELOCITY
+			});
+		}
+	},
+	mousewheel: function(e) {
+		var chg = 1 + (e.originalEvent.deltaY < 0 ? 0.1 : -0.1);
+		var newZoom = three60.getZoom() * chg;
+		three60.zoom(newZoom);
+	}
+};
+
+// assign initial listeners
+var initControls = function() {
+	$.each(listeners.hammer, function(k, v) {
+		$('#three60').hammer({
+			'prevent_default': true
+		}).on(k, v);
+	});
+	$('#three60').on('mousewheel', listeners.mousewheel);
+};
+
+var init = function(opts) {
+	three60.init({
+		fov: 96,
+		width: opts.$target.width(),
+		height: opts.$target.height(),
+		videoURL: opts.videoURL,
+		target: opts.$target[0],
+		callback: function() {
+			reset();
+			initControls();
+		}
+	});
+};
